49837ddd071356bc65038ddaeebd460a765be31b
Higo Ribeiro <HORBJN@gmail.com>
2017-12-15 21:57:45 -0200

    Ajustado o PropTypes da ProductList

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,11 +1,24 @@
+/* Core */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/* Presentational */
 import { ScrollView, ActivityIndicator } from 'react-native';
-
-import ProductItem from './components/ProductItem';
-
 import styles from './styles';
 
+/* Components */
+import ProductItem from './components/ProductItem';
+
 export default class List extends Component {
+  static propTypes = {
+    products: PropTypes.arrayOf(ProductItem.propTypes.product),
+    loading: PropTypes.bool,
+  }
+  static defaultProps = {
+    products: [],
+    loading: false,
+  }
+
   renderProducts() {
     return this.props.products.map(item => <ProductItem key={item.id} product={item} />);
   }
